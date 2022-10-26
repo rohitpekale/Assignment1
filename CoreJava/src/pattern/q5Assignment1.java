f4f93b682ce6a6327546a5c38741776de1f4ad7a
rohitpekale <rohitpekale@gmail.com>
2022-10-26 07:59:38 +0530

    change

--- CoreJava/src/pattern/q5Assignment1.java
+++ CoreJava/src/pattern/q5Assignment1.java
@@ -1,14 +1,16 @@
 package pattern;
 
-public class q4Assignment1 {
+public class q5Assignment1 {
 
 	public static void main(String[] args) {
-		int n=21;
+		
+		int n=15;
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
-				if(i==(n-1)        ||
-				   (i-j>=(n-1)/2)  ||					
-				  i+j>=(3*n-1)/2
+				if(i==0        ||
+					(i==(n-1))    ||
+				   (i+j<=(n-1)/2)  ||					
+				  i-j>=(n-1)/2
 					)
 				{
 				System.out.print("*");
@@ -19,7 +21,7 @@ public class q4Assignment1 {
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 }
