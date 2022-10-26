d121f920d48c67c1b1806d403ecc1f5920284347
rohitpekale <rohitpekale@gmail.com>
2022-10-26 07:52:26 +0530

    change

--- CoreJava/src/pattern/q4Assignment1.java
+++ CoreJava/src/pattern/q4Assignment1.java
@@ -3,13 +3,14 @@ package pattern;
 public class q4Assignment1 {
 
 	public static void main(String[] args) {
-		int n=20;
+		int n=21;
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
 				if(i==(n-1) ||
-				   (i-j==(n-1)/2) )
-				  // i+j>=(n+(n-1)/2)
-				//		)
+				   (i-j>=(n-1)/2)  ||
+					
+				  i+j>=(3*n-1)/2
+					)
 				{
 				System.out.print("*");
 				}
